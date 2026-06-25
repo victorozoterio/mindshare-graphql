40fe2e7023e011645c9d5568f0e701c717ccdd1a
Victor Ozoterio <victorozoterio@gmail.com>
2026-06-25 11:54:16 -0300

    feat: cria o FieldResolver para trazer os dados do author

--- src/resolvers/idea.resolver.ts
+++ src/resolvers/idea.resolver.ts
@@ -1,15 +1,25 @@
-import { Arg, Mutation, Resolver, UseMiddleware } from "type-graphql";
+import {
+  Arg,
+  FieldResolver,
+  Mutation,
+  Resolver,
+  Root,
+  UseMiddleware,
+} from "type-graphql";
 import { IdeaModel } from "../models/idea.model";
 import { IdeaService } from "../services/idea.service";
 import { CreateIdeaInput } from "../dtos/input/idea.input";
 import { IsAuth } from "../middlewares/auth.middleware";
 import { GqlUser } from "../graphql/decorators/user.decorator";
 import { User } from "@prisma";
+import { UserModel } from "src/models/user.model";
+import { UserService } from "src/services/user.service";
 
 @Resolver(() => IdeaModel)
 @UseMiddleware(IsAuth)
 export class IdeaResolver {
   private ideaService = new IdeaService();
+  private userService = new UserService();
 
   @Mutation(() => IdeaModel)
   async createIdea(
@@ -17,5 +27,10 @@ export class IdeaResolver {
     @GqlUser() user: User,
   ): Promise<IdeaModel> {
     return this.ideaService.createIdea(data, user.id);
+  }
+
+  @FieldResolver(() => UserModel)
+  async author(@Root() idea: IdeaModel): Promise<UserModel> {
+    return this.userService.findUser(idea.authorId);
   }
 }
